fdec2b03c2777348921d3568840636d9f4936c2b
rahul <rkc.98@outlook.com>
2021-08-30 13:00:13 +0530

    added dummydata

--- App.js
+++ App.js
@@ -1,21 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux';
+import { combineReducers, createStore } from 'redux';
 
+const rootReducer=combineReducers({
+  product:productsReducer
+})
+const store =createStore(rootReducer)
 export default function App() {
+  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+<Provider store={store} >
+
+</Provider>
+   
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+ 
 });

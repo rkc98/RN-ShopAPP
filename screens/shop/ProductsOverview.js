e8eaec9010ba149dd2be8c6bd0cf6b98d87317b0
Rahul <rkc.98@outlook.com>
2021-08-30 12:46:03 +0530

    basic setup

---
+++ screens/shop/ProductsOverview.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { FlatList, StyleSheet, Text, View } from 'react-native'
+
+const ProductsOverview = () => {
+    return (
+        <View>
+            <FlatList />
+        </View>
+    )
+}
+
+export default ProductsOverview
+
+const styles = StyleSheet.create({})
---
+++ store/reducers/products.js
@@ -0,0 +1,10 @@
+const initialState={
+    availableProducts:[],
+    userProducts:[]
+
+}
+
+export default (state=initialState,action)=>{
+    return state;
+
+}
